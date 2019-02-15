54364655816f4e6784bb98ec71eaec65b1038720
Andrew Krall <ankwdf@mail.missouri.edu>
2019-02-15 16:10:54 -0600

    Add files via upload

---
+++ reset_aduser_pws_savetofile.ps1
@@ -0,0 +1,13 @@
+Import-Module ActiveDirectory
+Add-Type -Assembly System.Web
+
+$group = Read-Host -Prompt 'Input the group you want to change the users passwords in'
+Write-Host "The group you selected is $group. Changing passwords now."
+
+$adGroupMemberList = Get-ADGroupMember -Identity "$group"
+
+foreach($user in $adGroupMemberList) {
+    $newPassword = [Web.Security.Membership]::GeneratePassword(32,8)
+    Set-ADAccountPassword -Identity $user -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $newPassword -Force)
+    Write-Output "UserID:$user `t Password:$newPassword" `n`n|FT -AutoSize >>NewPass.txt
+}
